12f8dd8db786914ef2d0bbfde683f82b0938f21d
Anuj-krSingh <122720966+Anuj-krSingh@users.noreply.github.com>
2024-04-15 05:52:23 +0000

    datatypes-summary done

---
+++ datatypes-summary.js
@@ -0,0 +1,54 @@
+// ========  Primitive  =========
+
+// 7 types: String, Number, Boolean, null, undefined, 
+//          Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false;
+// const outsideTemp = null;
+
+// let userEmail;
+// let userEmail1 = undefined;
+
+// console.log(userEmail);
+// console.log(userEmail1);
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+// console.log(id === anotherId);
+// console.log(id);
+// console.log(typeof id);
+// console.log(anotherId);
+// console.log(typeof anotherId);
+
+// const bigNumber = 1222334455667788765n;
+// console.log(bigNumber);
+
+// =============  Reference(Non-Primitive)  =============
+
+// Array, Objects, Functions
+
+// =====  Array   ======
+// const heros = ['Shaktimaan', 'Hatim', 'Karma']
+// console.log(heros);
+// console.log(typeof heros);
+
+// =====  Object  =======
+// let myObj = {
+//     name: 'John',
+//     age: 50
+// }
+// console.log(myObj);
+// console.log(typeof myObj);
+
+
+// =====  Function  =======
+// const myFunction = function() {
+//     console.log('Hello World');
+// }
+
+// console.log(typeof myFunction);
+// console.log(myFunction);
+
