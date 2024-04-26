7c08cd265274bc17d08e6520f6912bdb2fc271a1
Anuj-krSingh <122720966+Anuj-krSingh@users.noreply.github.com>
2024-04-26 07:24:51 +0000

    while & do while completed

---
+++ 08_iterations/while.js
@@ -0,0 +1,37 @@
+// -------  while loop  -------
+
+// let index = 0
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`);
+//     // index= index+2;
+//     index+=2;
+// }
+
+
+// let myArray = ['karma', 'shaktimaan', 'hatim']
+// let index = 0
+
+// while (index < myArray.length) {
+//     console.log(`Value is ${myArray[index]}`);
+//     // index++
+//     // index+=1
+//     index=index+1
+// }
+    
+
+// ------   do while loop  ------
+// ------  Example 1  -------
+// let score = 1
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while(score<=5)
+
+
+// ------  Example 2  -------
+// let score = 11
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while(score<=5)
+
